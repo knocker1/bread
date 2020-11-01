989fa31fdd5e6908a2cd706390360797bd5f461a
knocker1 <71930795+knocker1@users.noreply.github.com>
2020-11-01 15:44:41 +0530

    Update youtube_dl_echo.py

--- anydlbot/plugins/youtube_dl_echo.py
+++ anydlbot/plugins/youtube_dl_echo.py
@@ -202,17 +202,17 @@ async def echo(bot, update):
                 inline_keyboard.append(ikeyboard)
             if duration is not None:
                 cb_string_64 = "{}|{}|{}".format("audio", "64k", "mp3")
-                cb_string_128 = "{}|{}|{}".format("audio", "128k", "mp3")
-                cb_string = "{}|{}|{}".format("audio", "320k", "mp3")
+                #cb_string_128 = "{}|{}|{}".format("audio", "128k", "mp3")
+                #cb_string = "{}|{}|{}".format("audio", "320k", "mp3")
                 inline_keyboard.append([
                     InlineKeyboardButton(
-                        "MP3 " + "(" + "64 kbps" + ")", callback_data=cb_string_64.encode("UTF-8")),
-                    InlineKeyboardButton(
-                        "MP3 " + "(" + "128 kbps" + ")", callback_data=cb_string_128.encode("UTF-8"))
-                ])
-                inline_keyboard.append([
-                    InlineKeyboardButton(
-                        "MP3 " + "(" + "320 kbps" + ")", callback_data=cb_string.encode("UTF-8"))
+                        "MP3 " + "(" + "64 kbps" + ")", callback_data=cb_string_64.encode("UTF-8"))#,
+                   # InlineKeyboardButton(
+                      #  "MP3 " + "(" + "128 kbps" + ")", callback_data=cb_string_128.encode("UTF-8"))
+                #])
+                #inline_keyboard.append([
+                 #   InlineKeyboardButton(
+                  #      "MP3 " + "(" + "320 kbps" + ")", callback_data=cb_string.encode("UTF-8"))
                 ])
         else:
             format_id = response_json["format_id"]
